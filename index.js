89cf1d2b7b2189c4a24b38c98915638dc83cd9fc
Henry Neal <henry.neal0@gmail.com>
2019-02-25 18:05:49 -0500

    mvp finished

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
-const db = require('./database/dbConfig');
+// const db = require('./database/dbConfig');
 const Users = require('./users/user-module');
 
 const server = express();
@@ -13,6 +13,153 @@ server.use(cors());
 server.get('/', (req, res) => {
     res.send("It's alive!");
   });
+
+  server.post('/api/register', (req, res) => {
+    let user = req.body;
+  
+    // generate hash from user's password
+    const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  
+    // override user.password with hash
+    user.password = hash;
+  
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+  server.post('/api/login', (req, res) => {
+    let { username, password } = req.body;
+  
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        // check that passwords match
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!`, token:'this is a token' });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+  function restricted(req, res, next) {
+    const { username, password } = req.headers;
+  
+    if (username && password) {
+      Users.findBy({ username })
+        .first()
+        .then(user => {
+          if (user && bcrypt.compareSync(password, user.password)) {
+            next();
+          } else {
+            res.status(401).json({ message: 'Invalid Credentials' });
+          }
+        })
+        .catch(error => {
+          res.status(500).json({ message: 'Ran into an unexpected error' });
+        });
+    } else {
+      res.status(400).json({ message: 'No credentials provided' });
+    }
+  }
+  
+  // axios.get(url, { headers: { username, password } });
+  
+  // protect this route, only authenticated users should see it
+  server.get('/api/users', restricted, (req, res) => {
+    Users.find()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  });
+  
+  server.get('/users', restricted, async (req, res) => {
+    try {
+      const users = await Users.find();
+  
+      res.json(users);
+    } catch (error) {
+      res.send(error);
+    }
+  });
+
+// server.post('/api/register', (req, res) => {
+//     let user = req.body;
+//     const hash = bcrypt.hashSync(user.password, 10); // this generates a hash from the user's password. 
+//     user.password = hash; // overrides the user.password with the hash
+
+//     Users.add(user)
+//     .then(saved => {
+//         res.status(201).json(saved);
+//     })
+//     .catch(error => {
+//         res.status(500).json(error);
+//     });
+// });
+
+// server.post('/api/login', (req,res) => {
+//     let { username, password } = req.body;
+
+//     Users.findBy({ username })
+//     .first()
+//     .then(user => {
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//             res.status(200).json({message:`Greetings ${user.username}!`});
+//         }else{
+//             res.status(401).json({message:'Invalid Credentials'});
+//         }
+//     })
+//     .catch(error => {
+//         res.status(500).json(error);
+//     });
+// });
+
+// function restricted(req, res, next) {
+//     const { username, password } = req.headers;
+
+//     if(username && password) {
+//         Users.findBy({username})
+//         .first()
+//         .then(user => {
+//             if (user && bcrypt.compareSync(password, user.password)) {
+//                 next();
+//             }else{
+//                 res.status(401).json({message:"Invalid Credentials"});
+//             }
+//         })
+//         .catch(error => {
+//             res.status(500).json({message:'Ran into an unexpected error'});
+//         });
+//     } else {
+//         res.status(400).json({message:'No credentials provided'});
+//     }
+// }
+
+// server.get('/api/users', restricted, (req, res) => {
+//     Users.find()
+//     .then(users => {
+//         res.json(users);
+//     })
+//     .catch(err => res.send(err));
+// });
+
+// server.get('/users', restricted ,async (req,res) => {
+//     try {
+//         const users = await Users.find();
+//         res.json(users);
+//     } catch (error) {
+//         res.send(error);
+//     }
+// });
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
